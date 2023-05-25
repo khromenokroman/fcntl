f4378ce17cee4b89c4f727d0c3092b25abeab85d
khromenokroman <roma55592@yandex.ru>
2023-05-25 21:26:22 +0200

    add thread

--- fcntl.cpp
+++ fcntl.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <memory>
+#include <thread>
 
 class My_error final : public std::exception
 {
@@ -15,21 +16,34 @@ private:
 
 void writeFile(int fd, char *buf, size_t data_write, int buffer_size)
 {
-    for (; data_write != 0; data_write - buffer_size)
+    long count = 10;
+    int temp = data_write;
+    while (count != 0)
     {
-        size_t bytes_to_write = buffer_size;
-        for (int bytes_written = 0; bytes_written < bytes_to_write;)
+        data_write = temp;
+        for (; data_write != 0; data_write - buffer_size)
         {
-
-            int currently_written = write(fd, buf + bytes_written, bytes_to_write - bytes_written); // write
-            if (currently_written == -1)
+            size_t bytes_to_write = buffer_size;
+            for (int bytes_written = 0; bytes_written < bytes_to_write;)
             {
-                throw My_error("[ERROR] Cannot write file!");
+
+                int currently_written = write(fd, buf + bytes_written, bytes_to_write - bytes_written); // write
+                if (currently_written == -1)
+                {
+                    throw My_error("[ERROR] Cannot write file!");
+                }
+                bytes_written += currently_written;
             }
-            bytes_written += currently_written; 
+            data_write -= buffer_size;
+            sleep(3);
         }
-        data_write -= buffer_size;
+        count--;
     }
+}
+
+void printF(int i)
+{
+    std::cout << "HIII"<<std::endl;
 }
 
 int main()
@@ -37,8 +51,10 @@ int main()
     size_t buffer_size = 4096;
     size_t data_write = 100;
     std::unique_ptr<char[]> buf(new char[buffer_size]);
+    std::unique_ptr<char[]> buf2(new char[buffer_size]);
 
-    std::string str = "Helasdasdasfasdsfdfaafgdfgfasfgfbasgslkjbhjerakbnsdfkmcveankjgdvnlckas;dfmkvanjkb hgrdjfnv.kmclakdjfkgsbnj,dkv.mszlvln jfsk,gnmvb;cwledfvscdfbvclo";
+    std::string str = "[WARN] HACK ATEMPT: 123.123.123.123 port 80 (connection refused)\n";
+    std::string str2 = "[ACESS] INPUT: 12.113.13.100 port 80 (connection OK)\n";
 
     for (int i = 0; i < str.length(); i++)
     {
@@ -46,23 +62,31 @@ int main()
         std::cout << buf[i] << "\t";
     }
 
+    for (int i = 0; i < str2.length(); i++)
+    {
+        buf2[i] = str2[i];
+        std::cout << buf2[i] << "\t";
+    }
+
     std::cout << "\n";
 
     try
     {
-        int fd1 = open("data.txt", O_RDWR | O_CREAT| O_TRUNC | O_APPEND, 0777);
+        int fd1 = open("data.txt", O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0777);
         std::cout << "fd: " << fd1 << std::endl;
-        std::cout << "lseek: " << lseek(fd1,0,SEEK_END) << std::endl;
+        std::cout << "lseek: " << lseek(fd1, 0, SEEK_END) << std::endl;
 
-        writeFile(fd1, buf.get(), 30, 30);
-        std::cout << "lseek: " << lseek(fd1,0,SEEK_END) << std::endl;
-
+        // int numb = 10;
+        int size = str.length();
+        std::thread t1(writeFile, std::ref(fd1), buf.get(), std::ref(size),std::ref(size));
+        t1.detach();
+        // writeFile(fd1, buf.get(), str.length(), str.length());
+        // std::cout << "lseek: " << lseek(fd1, 0, SEEK_END) << std::endl;
 
         int fd2 = open("data.txt", O_RDWR | O_CREAT | O_APPEND, 0777);
-        writeFile(fd1, buf.get(), 30, 10);
+        writeFile(fd2, buf2.get(), str2.length(), str2.length());
         std::cout << "fd: " << fd1 << std::endl;
-        std::cout << "lseek: " << lseek(fd2,0,SEEK_END) << std::endl;
-
+        std::cout << "lseek: " << lseek(fd2, 0, SEEK_END) << std::endl;
 
         system("cat ./data.txt");
         std::cout << "\n";
@@ -77,6 +101,8 @@ int main()
         {
             std::cout << "[ERROR] Cannot close file2!" << std::endl;
         }
+        // t1.join();
+
     }
     catch (const std::exception &e)
     {
